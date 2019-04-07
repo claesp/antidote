841c7c22b5c477f28c086d94221a380fc427132d
Claes Persson <c@cp.se>
2019-04-07 14:21:58 +0200

    more styling and layout

--- user.go
+++ user.go
@@ -56,8 +56,41 @@ func userOpen(ctx *fasthttp.RequestCtx, ps fasthttprouter.Params) {
 			Group: libticket.TicketGroup{
 				ID:   3,
 				Name: "Managed Services"}}}
+	ticket2 := libticket.Ticket{
+		Number:      "INC000055823",
+		Title:       "Can't connect to website",
+		Description: "",
+		Contact: libticket.TicketContact{
+			ID:    1,
+			Name:  "Anders Andersson",
+			Email: "anders@example.com",
+			Phone: "+46850650212"},
+		Customer: libticket.TicketCustomer{
+			ID:   2,
+			Name: "ICA Gruppen AB"},
+		Created: ticket_created,
+		Ends:    ticket_ends,
+		Worklogs: []libticket.TicketWorklog{
+			libticket.TicketWorklog{
+				ID:      1,
+				Worklog: "Will start investigating this.",
+				Created: worklog1_time,
+				User: libticket.TicketUser{
+					ID:    1,
+					Name:  "Claes Persson",
+					Email: "claes.persson@ip-only.se"}}},
+		Category: "Firewalls",
+		Service:  "Data Communications",
+		Status:   libticket.TicketStatusInProgress,
+		Priority: libticket.TicketPriorityMedium,
+		Impact:   libticket.TicketImpactMedium,
+		Type:     libticket.TicketTypeIncident,
+		Assignee: libticket.TicketAssignee{
+			Group: libticket.TicketGroup{
+				ID:   3,
+				Name: "Managed Services"}}}
 
-	tickets := []libticket.TicketView{libticket.TicketView{ticket}}
+	tickets := []libticket.TicketView{libticket.TicketView{ticket}, libticket.TicketView{ticket2}}
 	p := &templates.UserOpenPage{Tickets: tickets}
 	ctx.SetContentType("text/html; charset=utf-8")
 	templates.WritePageTemplate(ctx, p)
